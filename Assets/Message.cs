9fde5393c259018d51ef5182f8ff1f93c02e3fd3
aurelam1 <miroaurela@gmail.com>
2022-01-29 14:14:36 +0200

    Message and order classes created.

---
+++ Assets/Message.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Message", menuName = "Message Data", order = 51)]
+public class Message : ScriptableObject
+{
+    public int round;
+    public int time;
+    [TextArea(3, 15)] public string message;
+}
