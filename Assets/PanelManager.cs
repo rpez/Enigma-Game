e75f12c65c3062f9bf6ff4fe051e2c6e9f133c8a
aurelam1 <miroaurela@gmail.com>
2022-01-29 11:50:12 +0200

    Encryption algorithm added.

--- Assets/PanelManager.cs
+++ Assets/PanelManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
+
+    const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    Dictionary<char, char> cipher;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cipher = CreateCipher();
+        string encryptedMessage1 = Encrypt("HELLO WORLD");
+        string encryptedMessage2 = Encrypt("HELLO AGAIN");
+        Debug.Log(encryptedMessage1);
+        Debug.Log(encryptedMessage2);
     }
 
     // Update is called once per frame
@@ -15,4 +23,33 @@ public class PanelManager : MonoBehaviour
     {
         
     }
+
+    Dictionary<char, char> CreateCipher()
+    {
+        // Map every letter to another random letter. Can map to same letter.
+
+        Dictionary<char, char> cipher = new Dictionary<char, char>();
+        string lettersLeft = alphabet;
+        foreach (char letter in alphabet)
+        {
+            int letterIndex = Random.Range(0, lettersLeft.Length - 1);
+            cipher.Add(letter, lettersLeft[letterIndex]);
+            lettersLeft.Remove(letterIndex);
+        }
+
+        return cipher;
+    }
+
+    string Encrypt(string message)
+    {
+        string encryptedMessage = "";
+        foreach (char letter in message)
+        {
+            if (this.cipher.ContainsKey(letter)) encryptedMessage += ("" + this.cipher[letter]);
+            else encryptedMessage += letter;
+
+        }
+        return encryptedMessage;
+    }
+
 }
